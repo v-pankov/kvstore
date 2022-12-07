1807109d974ace3a601a48468d211f3f14af0c73
Vladimir Pankov <vdrpkv@gmail.com>
2022-12-07 10:51:22 +0300

    Move key validation code back to single file

--- internal/core/entity/item/entity.go
+++ internal/core/entity/item/entity.go
@@ -1,6 +1,10 @@
 package item
 
 import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+
 	"github.com/vdrpkv/kvstore/internal/core/entity"
 )
 
@@ -17,4 +21,73 @@ type (
 
 func (k Key) EqualsTo(thatKey Key) bool {
 	return k == thatKey
+}
+
+func (k Key) Validate(v KeyValidator) error {
+	if len(k) > v.KeyMaxLen() {
+		return ErrKeyLength{
+			Max: v.KeyMaxLen(),
+			Got: len(k),
+		}
+	}
+
+	forbiddenCharIndex := strings.IndexAny(string(k), v.ForbiddenRunes())
+	if forbiddenCharIndex >= 0 {
+		forbiddenChar, _ := utf8.DecodeRuneInString(string(k)[forbiddenCharIndex:])
+		return ErrKeyForbiddenChar{
+			Rune:  forbiddenChar,
+			Index: forbiddenCharIndex,
+		}
+	}
+
+	return nil
+}
+
+type KeyValidator interface {
+	KeyMaxLen() int
+	ForbiddenRunes() string
+}
+
+func NewKeyValidator(keyMaxLen int, forbiddenRunes string) KeyValidator {
+	return keyValidator{
+		keyMaxLen:      keyMaxLen,
+		forbiddenRunes: forbiddenRunes,
+	}
+}
+
+type keyValidator struct {
+	keyMaxLen      int
+	forbiddenRunes string
+}
+
+func (v keyValidator) KeyMaxLen() int {
+	return v.keyMaxLen
+}
+
+func (v keyValidator) ForbiddenRunes() string {
+	return v.forbiddenRunes
+}
+
+type ErrKeyLength struct {
+	Max int
+	Got int
+}
+
+func (e ErrKeyLength) Error() string {
+	return fmt.Sprintf(
+		"key length [%d] is greater than the maximum [%d]",
+		e.Got, e.Max,
+	)
+}
+
+type ErrKeyForbiddenChar struct {
+	Rune  rune
+	Index int
+}
+
+func (e ErrKeyForbiddenChar) Error() string {
+	return fmt.Sprintf(
+		"key contains forbidden character [%s] at index [%d]",
+		string(e.Rune), e.Index,
+	)
 }
