80482646c4b8e518f310233df28b63176b29f352
Vladimir Pankov <vdrpkv@gmail.com>
2022-12-07 11:19:46 +0300

    Add test for item key validation

---
+++ internal/core/entity/item/entity_test.go
@@ -0,0 +1,120 @@
+package item
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/vdrpkv/kvstore/internal/mocks"
+)
+
+func Test_Key_Validate(t *testing.T) {
+	var (
+		stubKey               = "stubkey"
+		stubRunesNotInStubKey = "\t"
+	)
+
+	type (
+		testCaseGiveCallsKeyMaxLen struct {
+			Returns int
+			Called  bool
+		}
+		testCaseGiveCallsForbiddenRunes struct {
+			Returns string
+			Called  bool
+		}
+		testCaseGiveCalls struct {
+			keyMaxLen      testCaseGiveCallsKeyMaxLen
+			forbiddenRunes testCaseGiveCallsForbiddenRunes
+		}
+		testCaseGive struct {
+			key   string
+			calls testCaseGiveCalls
+		}
+		testCase struct {
+			name string
+			give testCaseGive
+			want error
+		}
+	)
+	for _, tc := range []testCase{
+		{
+			"success",
+			testCaseGive{
+				key: stubKey,
+				calls: testCaseGiveCalls{
+					keyMaxLen: testCaseGiveCallsKeyMaxLen{
+						Returns: len(stubKey),
+						Called:  true,
+					},
+					forbiddenRunes: testCaseGiveCallsForbiddenRunes{
+						Returns: stubRunesNotInStubKey,
+						Called:  true,
+					},
+				},
+			},
+			nil,
+		},
+		{
+			"key max length error",
+			testCaseGive{
+				key: stubKey,
+				calls: testCaseGiveCalls{
+					keyMaxLen: testCaseGiveCallsKeyMaxLen{
+						Returns: len(stubKey) - 1,
+						Called:  true,
+					},
+				},
+			},
+			ErrKeyLength{
+				Max: len(stubKey) - 1,
+				Got: len(stubKey),
+			},
+		},
+		{
+			"forbidden runes error",
+			testCaseGive{
+				key: stubKey + stubRunesNotInStubKey,
+				calls: testCaseGiveCalls{
+					keyMaxLen: testCaseGiveCallsKeyMaxLen{
+						Returns: len(stubKey) + len(stubRunesNotInStubKey),
+						Called:  true,
+					},
+					forbiddenRunes: testCaseGiveCallsForbiddenRunes{
+						Returns: stubRunesNotInStubKey,
+						Called:  true,
+					},
+				},
+			},
+			ErrKeyForbiddenChar{
+				Rune:  []rune(stubRunesNotInStubKey)[0],
+				Index: len(stubKey),
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			keyValidatorMock := mocks.NewEntityItemKeyValidator(t)
+
+			if tc.give.calls.keyMaxLen.Called {
+				keyValidatorMock.On("KeyMaxLen").Return(
+					tc.give.calls.keyMaxLen.Returns,
+				)
+			}
+
+			if tc.give.calls.forbiddenRunes.Called {
+				keyValidatorMock.On("ForbiddenRunes").Return(
+					tc.give.calls.forbiddenRunes.Returns,
+				)
+			}
+
+			gotErr := Key(tc.give.key).Validate(keyValidatorMock)
+
+			if tc.want == nil {
+				require.NoError(t, gotErr)
+			}
+
+			if tc.want != nil {
+				require.ErrorIs(t, gotErr, tc.want)
+			}
+		})
+	}
+}
